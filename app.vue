e3d43bb93d1fc33568961322f65ec783453dcd83
trankhacbinhduong <trankhacbinhduong@gmail.com>
2023-06-14 16:12:56 +0700

    :zap: Add file-based routing configuration

--- app.vue
+++ app.vue
@@ -1,22 +1,7 @@
 <template>
-  <div class="home-page">
-    <h1 class="title">
-      Task Completion Tracking
-    </h1>
+  <div>
+    <NuxtLayout>
+      <NuxtPage />
+    </NuxtLayout>
   </div>
 </template>
-
-<style lang="scss" scoped>
-.home-page {
-  display: flex;
-  align-items: center;
-  justify-content: center;
-  width: 100%;
-  height: 100vh;
-
-  > .title {
-    text-align: center;
-    color: $primary-color;
-  }
-}
-</style>
