9d1f88df43349f19b9952d0b27f546ab24c6487c
trankhacbinhduong <trankhacbinhduong@gmail.com>
2023-06-15 17:25:41 +0700

    :bug: Fix fallback route auth middleware

--- middleware/auth.ts
+++ middleware/auth.ts
@@ -1,6 +1,7 @@
-export default defineNuxtRouteMiddleware((to, _from) => {
+export default defineNuxtRouteMiddleware(async (to, _from) => {
   const user = useSupabaseUser()
 
+  const fallbackRoute = to.path === '/' ? 'login' : `login?redirectTo=${to.fullPath}`
   if (!user.value)
-    return navigateTo(to.path === '/' ? 'login' : `login?redirectTo=${to.fullPath}`)
+    return navigateTo(fallbackRoute)
 })
