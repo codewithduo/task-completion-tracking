c2df2f3dc4cf5374592b74eee41fa9a9ba07636c
trankhacbinhduong <trankhacbinhduong@gmail.com>
2023-06-15 17:47:41 +0700

    :zap: Update fallback route auth middleware

--- middleware/auth.ts
+++ middleware/auth.ts
@@ -1,7 +1,7 @@
 export default defineNuxtRouteMiddleware(async (to, _from) => {
   const user = useSupabaseUser()
 
-  const fallbackRoute = to.path === '/' ? 'login' : `login?redirectTo=${to.fullPath}`
+  const fallbackRoute = `login?redirectTo=${to.fullPath}`
   if (!user.value)
     return navigateTo(fallbackRoute)
 })
