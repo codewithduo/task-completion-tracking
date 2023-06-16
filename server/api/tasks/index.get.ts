822da210ba9c3ae8b1f9b873aaa224325ec2c51c
trankhacbinhduong <trankhacbinhduong@gmail.com>
2023-06-16 08:24:57 +0700

    :sparkles: Implement get tasks api

---
+++ server/api/tasks/index.get.ts
@@ -0,0 +1,14 @@
+import type { H3Event } from 'h3'
+import type { Task } from '@prisma/client'
+import { REQUEST_USER_KEY } from '../../constants'
+import prismaClient from '../../database/prismaClient'
+
+export default defineEventHandler(async (event: H3Event): Promise<Task[]> => {
+  const { email: userEmail } = event.context[REQUEST_USER_KEY]
+
+  const tasks = await prismaClient.task.findMany({
+    where: { userEmail },
+  })
+
+  return tasks
+})
