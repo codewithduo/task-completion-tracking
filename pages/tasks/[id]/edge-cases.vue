6ebdcf22ae5b07aecc4b7d8d14162a2f3bd2fbec
trankhacbinhduong <trankhacbinhduong@gmail.com>
2023-07-03 10:45:39 +0700

    :sparkles: Implement task edge cases page

--- pages/tasks/[id]/edge-cases.vue
+++ pages/tasks/[id]/edge-cases.vue
@@ -1,23 +1,106 @@
 <script setup lang="ts">
+import type { TaskRelation } from '~/server/api/tasks/[id].get'
 
+const props = defineProps<{
+  task: TaskRelation
+}>()
+
+const columns = [
+  {
+    label: 'Id',
+    value: 'id',
+  },
+  {
+    label: 'Name',
+    value: 'name',
+  },
+  {
+    label: 'Solution',
+    value: 'solution',
+  },
+  {
+    label: '',
+    value: 'action',
+  },
+]
+
+const edgeCases = computed(() => {
+  return props.task.taskEdgeCases
+})
 </script>
 
 <template>
   <div class="task-edge-cases">
-    <form class="edge-cases">
-      <fieldset class="fieldset">
-        <legend class="legend">
-          Think what none trivial state the task might have. Describe them and their handling
-        </legend>
-      </fieldset>
-    </form>
+    <div class="edge-cases">
+      <table class="table">
+        <caption class="caption">
+          Edge Cases
+          <p class="extra">
+            Think what none trivial state the task might have. Describe them and their solution.
+          </p>
+        </caption>
+        <thead class="head">
+          <tr class="row">
+            <th v-for="column in columns" :key="column.value" scope="col" class="cell">
+              {{ column.label }}
+            </th>
+          </tr>
+        </thead>
+        <tbody class="body">
+          <tr v-for="edgeCase in edgeCases" :key="edgeCase.id" class="row">
+            <td scope="row" class="cell">
+              #{{ edgeCase.id }}
+            </td>
+            <td scope="row" class="cell">
+              {{ edgeCase.name }}
+            </td>
+            <td class="cell">
+              {{ edgeCase.solution }}
+            </td>
+            <td class="cell text-right">
+              <a href="#" class="font-medium text-blue-600 hover:underline"><Icon name="uil:edit" /> Edit</a>
+            </td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
   </div>
 </template>
 
-<style scoped>
+<style scoped lang="scss">
 .task-edge-cases {
-  > .edge-cases {
-    padding: 16px;
+  height: 100%;
+  @apply p-4
+}
+
+.edge-cases {
+  @apply relative overflow-x-auto sm:rounded-lg h-full;
+
+  > .table {
+    @apply w-full text-sm text-left text-gray-500;
+  }
+
+  > .table > .caption {
+    @apply py-1 text-lg font-semibold text-left text-gray-900 bg-white
+  }
+
+  > .table > .caption > .extra {
+    @apply mb-3 text-sm font-normal text-gray-500
+  }
+
+  > .table > .head {
+    @apply text-xs text-gray-700 uppercase bg-gray-50
+  }
+
+  > .table > .head > .row > .cell {
+    @apply px-6 py-4
+  }
+
+  > .table > .body > .row {
+    @apply bg-white border-b
+  }
+  > .table > .body > .row > .cell {
+    @apply px-6 py-4 font-medium text-gray-900 whitespace-nowrap
   }
 }
 </style>
