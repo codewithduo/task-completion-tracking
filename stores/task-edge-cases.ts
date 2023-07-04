23c1aaddf55fa74a8cc2dcf5963763819ff1a2ef
trankhacbinhduong <trankhacbinhduong@gmail.com>
2023-07-04 08:10:03 +0700

    :sparkles: Implement fetch taskEdgeCases by taskId

--- stores/task-edge-cases.ts
+++ stores/task-edge-cases.ts
@@ -2,8 +2,25 @@ import type { TaskEdgeCase } from '@prisma/client'
 import type { CreateTaskEdgeCaseDto } from '~/server/api/task-edge-cases/index.post'
 
 export const useTaskEdgeCaseStore = defineStore('task-edge-cases', () => {
+  const taskEdgeCases = ref<TaskEdgeCase[]>([])
+
+  const fetchTaskEdgeCases = async (taskId: number) => {
+    const { data, error } = await useFetch<TaskEdgeCase[]>(`/api/task-edge-cases?taskId=${taskId}`, {
+      headers: useRequestHeaders(['cookie']),
+    })
+
+    if (error.value) {
+      return useNotification({
+        message: error.value.message,
+        type: 'error',
+      })
+    }
+
+    taskEdgeCases.value = data.value as TaskEdgeCase[]
+  }
+
   const createTaskEdgeCase = async (createTaskEdgeCaseData: CreateTaskEdgeCaseDto) => {
-    const { error } = await useFetch<TaskEdgeCase>('/api/task-edge-cases', {
+    const { data, error } = await useFetch<TaskEdgeCase>('/api/task-edge-cases', {
       method: 'POST',
       headers: useRequestHeaders(['cookie']),
       body: createTaskEdgeCaseData,
@@ -16,10 +33,12 @@ export const useTaskEdgeCaseStore = defineStore('task-edge-cases', () => {
       })
     }
 
+    taskEdgeCases.value.push(data.value as TaskEdgeCase)
+
     useNotification({
       message: 'Task edge case created successfully!',
     })
   }
 
-  return { createTaskEdgeCase }
+  return { createTaskEdgeCase, fetchTaskEdgeCases, taskEdgeCases }
 })
