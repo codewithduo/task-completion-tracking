27a975624cf2cd612646a6f597b3b6193088c9e4
trankhacbinhduong <trankhacbinhduong@gmail.com>
2023-07-05 15:07:01 +0700

    :sparkles: Refactor create and delete task feature

--- pages/tasks/index.vue
+++ pages/tasks/index.vue
@@ -44,9 +44,7 @@ const tasksStore = useTasksStore()
 const { tasks } = storeToRefs(tasksStore)
 await tasksStore.fetchTasks()
 
-const isVisible = ref(false)
-const showModal = () => (isVisible.value = true)
-const hideModal = () => (isVisible.value = false)
+const { isVisible, showModal, hideModal } = useModal()
 
 const createTaskData = reactive<CreateTaskData>({
   name: '',
@@ -142,6 +140,55 @@ function handleNavigateToTask(id: number) {
       <button type="button" class="create-button" @click="showModal">
         <Icon name="uil:plus" size="20" />
       </button>
+      <div v-if="isVisible" class="modal-container">
+        <div class="overlay">
+          <!-- Modal content -->
+          <div class="content">
+            <button type="button" class="button-close" @click="hideModal">
+              <Icon name="uil:multiply" size="20" />
+            </button>
+            <div class="card">
+              <h3 class="header">
+                Add new Task
+              </h3>
+              <form class="body">
+                <div class="form-item">
+                  <label for="name" class="label">Name</label>
+                  <textarea
+                    id="name"
+                    v-model="createTaskData.name"
+                    rows="3"
+                    class="input"
+                    placeholder="Describe task name here..."
+                  />
+                </div>
+                <div class="form-item">
+                  <label for="solution" class="label">Description</label>
+                  <textarea
+                    id="solution"
+                    v-model="createTaskData.description"
+                    rows="6"
+                    class="input"
+                    placeholder="Describe task description here..."
+                  />
+                </div>
+              </form>
+              <div class="footer">
+                <button
+                  type="submit"
+                  class="button-save"
+                  @click="handleCreateTask"
+                >
+                  Save
+                </button>
+                <button type="button" class="button-cancel" @click="hideModal">
+                  Cancel
+                </button>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   </div>
 </template>
@@ -188,6 +235,52 @@ function handleNavigateToTask(id: number) {
 
   > .create-button {
     @apply absolute top-6 right-7 flex items-center justify-center text-white bg-blue-600 rounded-full w-10 h-10 hover:bg-blue-700;
+  }
+}
+
+.modal-container {
+  @apply flex justify-center items-center bg-[rgba(147,147,147,0.5)] fixed top-0 left-0 right-0 bottom-0 w-full z-10 p-4 overflow-x-hidden overflow-y-auto md:inset-0 h-full max-h-full;
+
+  > .overlay {
+    @apply relative w-full max-w-2xl max-h-full;
+  }
+
+  > .overlay > .content {
+    @apply relative bg-white rounded-lg shadow w-[100%];
+  }
+
+  > .overlay > .content > .button-close {
+    @apply absolute top-3 right-2.5 text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm p-1.5 ml-auto inline-flex items-center;
+  }
+
+  > .overlay > .content > .card {
+    @apply px-6 py-6 lg:px-8;
+  }
+
+  > .overlay > .content > .card > .header {
+    @apply mb-4 text-xl font-medium text-gray-900;
+  }
+  > .overlay > .content > .card > .body {
+    @apply space-y-6;
+  }
+
+  > .overlay > .content > .card > .body > .form-item > .label {
+    @apply block mb-2 text-sm font-medium text-gray-900;
+  }
+
+  > .overlay > .content > .card > .body > .form-item > .input {
+    @apply block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500;
+  }
+
+  > .overlay > .content > .card > .footer {
+    @apply text-right mt-4;
+  }
+  > .overlay > .content > .card > .footer > .button-save {
+    @apply mr-2 text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center;
+  }
+
+  > .overlay > .content > .card > .footer > .button-cancel {
+    @apply py-2.5 px-5 text-sm font-medium text-gray-900 focus:outline-none bg-white rounded-lg border border-gray-200 hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-4 focus:ring-gray-200;
   }
 }
 </style>
