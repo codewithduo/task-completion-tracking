264d6c34d955f000d4c6bfa980c743fc5dfeb2b9
trankhacbinhduong <trankhacbinhduong@gmail.com>
2023-07-05 11:52:20 +0700

    :wheelchair: Improve accessibility with title element

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -26,4 +26,15 @@ export default defineNuxtConfig({
       'storeToRefs',
     ],
   },
+  app: {
+    head: {
+      title: 'Task Completion Tracking',
+      meta: [
+        { charset: 'utf-8' },
+        { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+        { hid: 'description', name: 'description', content: '' },
+      ],
+      link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }],
+    },
+  },
 })
