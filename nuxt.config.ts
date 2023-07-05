fb53c4837ccfde5d26118509f09b84d2450c9408
trankhacbinhduong <trankhacbinhduong@gmail.com>
2023-07-05 11:57:32 +0700

    :wheelchair: Improve content best practice with meta description

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -35,7 +35,7 @@ export default defineNuxtConfig({
       meta: [
         { charset: 'utf-8' },
         { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-        { hid: 'description', name: 'description', content: '' },
+        { hid: 'description', name: 'description', content: 'Tracking your task professionally' },
       ],
       link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }],
     },
